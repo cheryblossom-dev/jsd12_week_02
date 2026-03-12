e9f26a3b5babbee77676a888fbe2cddb10b6d22a
cheryblossom-dev <kaaashaaa9@gmail.com>
2026-03-12 16:52:24 +0700

    Delete 03_orders.sql

--- 03_orders.sql
+++
@@ -1,8 +0,0 @@
--- Clear existing data to prevent duplication
-TRUNCATE TABLE orders RESTART IDENTITY CASCADE;
-
--- Mock Data for Suppliers
-INSERT INTO orders (orders_id,customer_id,date) VALUES
-(1,1,01022026),
-(2,2,02022026),
-(3,3,03022026);
