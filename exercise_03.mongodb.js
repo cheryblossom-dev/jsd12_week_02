a963fe77c96cab6ee6b9b501c302137c0ba26918
cheryblossom-dev <kaaashaaa9@gmail.com>
2026-03-16 21:30:22 +0700

    Delete exercise_03.mongodb.js

--- exercise_03.mongodb.js
+++
@@ -1,6 +0,0 @@
-use('sample_mflix')
-db.theaters.find({"location.address.state":"AL"}).count()
-use('sample_mflix')
-db.theaters.find({"location.address.city":"La Quinta"}).count()
-use('sample_mflix')
-db.theaters.findOne()
