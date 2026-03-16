19507be280ffd433a8701ad4f7945c3954097da2
cheryblossom-dev <kaaashaaa9@gmail.com>
2026-03-16 21:51:16 +0700

    Delete 04_mongodb-sample-mfix/myecommerce directory

--- 04_mongodb-sample-mfix/myecommerce/02_product.sql
+++
@@ -1,8 +0,0 @@
--- Clear existing data to prevent duplication
-TRUNCATE TABLE product RESTART IDENTITY CASCADE;
-
--- Mock Data for Suppliers
-INSERT INTO product (product_id, price,count,product_name) VALUES
-(1, 199,10,'protein'),
-(2, 299,20,'fish oil'),
-(3, 399,30,'vitaminc');
