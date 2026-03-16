60690076396c82651e8d086c53321d75bce9fe73
cheryblossom-dev <kaaashaaa9@gmail.com>
2026-03-16 21:30:00 +0700

    Delete exercise_01.mongodb.js

--- exercise_01.mongodb.js
+++
@@ -1,5 +0,0 @@
-use('sample_mflix')
-db.comments.find({})
-db.comments.find({_id:ObjectId('5a9427648b0beebeb69579f5')})
-db.comments.findOne({email:'john_bishop@fakegmail.com'})
-db.comments.findOne({name:'John Bishop'})
